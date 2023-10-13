d537f88a85daa9b794ae141c8cfa429d0d3f79c4
brodsa <brodinova@gmail.com>
2023-10-13 12:02:56 +0000

    feat: add how-it-works template, url, view and style it

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Index
+from .views import Index, HowItWorks
 
 urlpatterns = [
-    path('', Index.as_view(), name='home')
+    path('', Index.as_view(), name='home'),
+    path('how', HowItWorks.as_view(), name='how')
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.views.generic import TemplateView
 class Index(TemplateView):
     """ Class for the index view """
     template_name = 'home/index.html'
+
+
+class HowItWorks(TemplateView):
+    """Class for how-it-works view """
+    template_name = 'home/how.html'
