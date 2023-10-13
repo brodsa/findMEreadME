63207bbb5efe20df9c5050190efceb0e4b384218
brodsa <brodinova@gmail.com>
2023-10-13 13:19:51 +0000

    test: write test method for how-it-worsk view and http request status

--- home/tests.py
+++ home/tests.py
@@ -9,3 +9,9 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
 
+    def test_how_page(self):
+        """Test http request and view of how-it-works page"""
+        response = self.client.get('/how')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/how.html')
+
