bcdabca610885f37830680254824ac856f9ade06
brodsa <brodinova@gmail.com>
2023-12-10 19:48:06 +0000

    feat: add the cities in the json into the dropdown of the form Book contribution

--- books/helpers.py
+++ books/helpers.py
@@ -1,6 +1,7 @@
 import string
 import random
 import os
+import json
 
 
 def get_id():
@@ -28,3 +29,43 @@ def generate_key(id_book, key_length=8):
     new_id = int(id_seed.strip()) + 1
     update_id(str(new_id))
     return key
+
+
+def convert_geonames():
+    print('start')
+    """ 
+        A function to convert original countries json into djnago json format.
+        Manually call: 
+            cd books
+            python3 helpers.py convert_js_to_djanfo_js 
+        
+    """
+    # open source data
+    with open("./books/data/geonames.json") as file:
+        data_js = json.load(file)
+        records = []
+        i = 0
+        # create records for django json format
+        print('start reading')
+        for item in data_js:
+            if item["cou_name_en"] in ('Ireland','Czech Republic','Austria'):
+                record = {
+                    'model': 'books.city',
+                    'pk': i,
+                    'fields':{
+                        'city': item["name"],
+                        'country': item["cou_name_en"]
+                        } 
+                }
+                i = i +1
+                records.append(record)
+            
+        # write the new json into new json file
+        print('write json')
+        with open('./books/fixtures/cities.json', 'w') as django_js:
+            django_js.write(json.dumps(records))
+
+# convert_geonames()
+
+
+            
